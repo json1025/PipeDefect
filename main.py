f790803db36b23fabe84728b9192032d25201dc6
json1025 <jasonqiuy@gmail.com>
2019-02-16 22:00:15 -0500

    added file writing functionality

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from pathlib import Path
 import fileprocessing as fp
 import datatransforming as dt
 
+
 data_folder = Path('')
 file = data_folder / 'StraightPipe_SingleDefect - Sheet1.csv'
-# destination_file = data_folder / 'PipeDefects_DataTransformation.csv'
-destination_file = data_folder / 'test.csv'
 
 data = fp.read_file(file)
-
 dt.DataTransformation.init_locations(data[-1])
-a = dt.DataTransformation(data[0])
-a.process_data()
-data = a.final_data
+data1 = data[:-1]  # remove location data
+sensors = {data1[i][0]: dt.DataTransformation(data1[i]) for i in range(len(data1))}
 
-fp.write_file(destination_file, data)
+for s_name, s in sensors.items():
+    s.process_data()
+    s_data = s.final_data
+    destination_file = data_folder / (s_name + '_PipeDefects_DataTransformation.csv')
+    fp.write_file(destination_file, s_data)
